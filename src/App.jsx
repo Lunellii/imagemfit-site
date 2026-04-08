79fe1a339b4de5bbb317d409f5f4171d14af2395
Lunellii <Lunellii@users.noreply.github.com>
2026-04-08 08:19:14 -0300

    chore: add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { queryClientInstance } from "@/lib/query-client";
 import PageNotFound from "@/lib/PageNotFound";
 import Layout from "@/components/Layout";
