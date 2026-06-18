5e1a8bffc13983d8e4faa9c13fa0dc095897cfd0
Gustavo Lunelli Gonçalves <gustavolunelli54@gmail.com>
2026-06-18 11:40:35 -0300

    Redirect hidden orçamento paths to static page

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const hiddenQuotePaths = new Set(["/orcamento", "/orcamento/", "/ocamento", "/ocamento/"]);
+
+if (hiddenQuotePaths.has(window.location.pathname)) {
+  window.location.replace("/orcamento/index.html");
+} else {
+  ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+}
